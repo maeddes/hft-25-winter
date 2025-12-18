e0b67054684f177daca8c60b03e918351c1fb5c6
Matthias Haeussler <maeddes@gmail.com>
2025-12-18 11:21:48 +0100

    profiles ctd

--- project/backend-springboot/src/main/java/com/example/shop/ShopApplication.java
+++ project/backend-springboot/src/main/java/com/example/shop/ShopApplication.java
@@ -1,10 +1,21 @@
 package com.example.shop;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ShopApplication {
+
+	@Value("${spring.profiles.active:default}")
+    private String activeProfile;
+
+    @PostConstruct
+    public void log() {
+        System.out.println("Active Spring profile: " + activeProfile);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
